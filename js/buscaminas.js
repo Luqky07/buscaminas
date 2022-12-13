28fa0064c8a7fb81be5327cf1bee702f799b3f5e
Luqky07 <sergiolc3003@gmail.com>
2022-12-13 08:37:21 +0100

    Guardar en indexeddb

--- js/buscaminas.js
+++ js/buscaminas.js
@@ -1,13 +1,29 @@
 //Elementos del DOM
 const campo = document.getElementById("campo");
 const form = document.getElementById("form");
-const fin = document.getElementById("terminado");
-const cBanderas = document.getElementById("resultados");
+const win = document.getElementById("win");
+const losse = document.getElementById("loose");
+const cBanderas = document.getElementById("cBanderas");
+const time = document.getElementById("time");
+const info = document.getElementById("info");
+const indexedDB = window.indexedDB;
+const marcador = document.getElementById("marcador");
+const bMarcdor = document.getElementById("bMarcador");
+const footer = document.getElementById("footer");
 
+let db;
+let firstOpt = true;
 let bombas = [];
 let casillasRevisadas = [];
 let globalSize = 0;
 let banderas = 0;
+let intervalo;
+let reloj = {
+    segundos: 0,
+    minutos: 0,
+    horas: 0
+}
+let dificultad;
 
 //Funciones
 function generarCampo(size, bombs) {
@@ -37,6 +53,21 @@ function events(size) {
     }
 }
 
+function cronometro() {
+    if (reloj.segundos++ == 60) {
+        reloj.segundos = 0;
+        if (reloj.minutos++ == 60) {
+            reloj.minutos = 0;
+            reloj.horas++;
+        }
+    }
+    if (reloj.segundos < 10 != "00") reloj.segundos = "0" + Number(reloj.segundos);
+    if (reloj.minutos < 10 != "00") reloj.minutos = "0" + Number(reloj.minutos);
+    if (reloj.horas < 10 != "00") reloj.horas = "0" + Number(reloj.horas);
+    if (reloj.horas != 0) time.innerHTML = "Tiempo " + reloj.horas + ":" + reloj.minutos + ":" + reloj.segundos;
+    else time.innerHTML = "Tiempo " + reloj.minutos + ":" + reloj.segundos;
+}
+
 function ponerBandera(e) {
     let id = document.getElementById(e.target.id);
     if (id.classList[1] == "flag") {
@@ -48,10 +79,14 @@ function ponerBandera(e) {
             banderas--;
         }
     }
-    cBanderas.children[1].innerHTML = banderas;
+    cBanderas.innerHTML = banderas;
 }
 
 function comprobarCasilla(e) {
+    if (firstOpt == true) {
+        intervalo = setInterval(cronometro, 1000);
+        firstOpt = false;
+    }
     let id = document.getElementById(e.target.id);
     if (!id.classList.contains("flag")) {
         id.className = "vacio";
@@ -114,7 +149,7 @@ function comprobarAlrededor(id, size) {
             newCords = cords[0] + "-" + (Number(cords[1]) + 1);
             if ((Number(cords[1]) + 1) < size && !casillasRevisadas.includes(newCords)) comprobarAlrededor(newCords, size);
         }
-        if (casillasRevisadas.length == (size * size) - bombas.length){
+        if (casillasRevisadas.length == (size * size) - bombas.length) {
             console.log(casillasRevisadas);
             generarFin("ganador");
         }
@@ -122,6 +157,7 @@ function comprobarAlrededor(id, size) {
 }
 
 function generarFin(estado) {
+    clearInterval(intervalo);
     for (let fila = 0; fila < globalSize; fila++) {
         for (let columna = 0; columna < globalSize; columna++) {
             let id = document.getElementById(columna + "-" + fila);
@@ -129,10 +165,14 @@ function generarFin(estado) {
             id.removeEventListener("contextmenu", ponerBandera);
         }
     }
-    if (estado == "ganador") fin.innerHTML = "Felicidades te has pasado el juego.";
-    else fin.innerHTML = "Ni Carlos juega tan mal como tú."
-}
+    if (estado == "ganador") {
+        win.className = "msg";
+        if (reloj.horas != 0) win.children[1].innerHTML = time.innerHTML;
+        else win.children[1].innerHTML = time.innerHTML;
 
+    }
+    else loose.className = "msg";
+}
 function generarBombas(size, bombs) {
     let contBomb = 0;
     do {
@@ -145,17 +185,85 @@ function generarBombas(size, bombs) {
     } while (contBomb < bombs)
 }
 
-//Eventos
-form.addEventListener("submit", (e) => {
-    cBanderas.removeAttribute("class");
-    fin.innerHTML = "";
+function reiniciarJuego() {
+    losse.className = "invisible"
+    win.className = "invisible";
+    firstOpt = true;
+    reloj.segundos = "00";
+    reloj.minutos = "00";
+    reloj.horas = "00";
+    time.innerHTML = "Tiempo " + reloj.minutos + ":" + reloj.segundos;
+    clearInterval(intervalo);
+    info.removeAttribute("class");
     bombas = [];
     casillasRevisadas = [];
+}
+
+function addData(data) {
+    let transaction = db.transaction([dificultad], "readwrite");
+    let objectStore = transaction.objectStore(dificultad);
+    let request = objectStore.add(data);
+}
+
+function readData() {
+    let transaction = db.transaction([dificultad], "readonly");
+    let objectStore = transaction.objectStore(dificultad);
+    let request = objectStore.openCursor();
+    let res = ""
+    request.onsuccess = (e) => {
+        let cursor = e.target.result;
+        if (cursor) {
+            res += "<li>" + cursor.value.name + " / " + cursor.value.time + "</li>";
+            cursor.continue();
+        }
+        marcador.children[1].innerHTML = res;
+    }
+    marcador.className = "marcador";
+}
+
+if (indexedDB) {
+    let request = indexedDB.open("playerList", 1)
+    request.onupgradeneeded = () => {
+        db = request.result;
+        console.log("Database creada");
+        let dificultades = ["8x8 5 minas", "16x16 10 minas", "8x8 10 minas", "16x16 20 minas", "8x8 20 minas", "16x16 40 minas"];
+        for (d of dificultades) {
+            db.createObjectStore(d, { autoIncrement: true });
+        }
+    }
+    request.onsuccess = () => {
+        db = request.result
+        console.log("Database abierta");
+        win.children[2].addEventListener("submit", (e) => {
+            let data = {
+                name: e.target.name.value,
+                mode: dificultad,
+                time: reloj.horas + ":" + reloj.minutos + ":" + reloj.segundos
+            }
+            addData(data);
+            e.target.name.value = "";
+            win.className = "invisible";
+        });
+    }
+    request.onerror = (error) => {
+        console.log("Error");
+    }
+}
+
+//Eventos
+form.addEventListener("submit", (e) => {
+    reiniciarJuego();
+    footer.className = "footer";
     e.preventDefault();
-    let respuesta = e.target.size.value;
-    let array = respuesta.split(" ");
+    dificultad = e.target.size.value;
+    let array = dificultad.split(" ");
     let size = array[0].split("x");
     banderas = array[1];
-    cBanderas.children[1].innerHTML = banderas;
+    cBanderas.innerHTML = banderas;
     generarCampo(size[0], array[1]);
 })
+
+losse.children[0].addEventListener("click", () => { losse.className = "invisible" })
+marcador.children[0].addEventListener("click", () => { marcador.className = "invisible" })
+
+bMarcdor.addEventListener("click", readData);
